2d004e7909f94c5898a04ec4f5e4f17898abf1b3
topi0247 <karubona.ra247@gmail.com>
2024-10-19 18:04:48 +0900

    compolete todo app

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -77,3 +77,49 @@ func todoEdit(w http.ResponseWriter, r *http.Request, id int) {
 		generateHTML(w, t, "layout", "private_navbar", "todo_edit")
 	}
 }
+
+func todoUpdate(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		content := r.PostFormValue("content")
+		t := &models.Todo{
+			ID:      id,
+			Content: content,
+			UserID:  user.ID,
+		}
+		if err := t.UpdateTodo(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", http.StatusFound)
+	}
+}
+
+func todoDelete(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+	} else {
+		_, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if err := t.DeleteTodo(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", http.StatusFound)
+	}
+}
